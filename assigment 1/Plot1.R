f2bfd2423927129d2527cbed499b1c1a0126584b
MiataIra <iryna.kulagina@outlook.com>
2020-02-13 14:03:28 +0100

    Create Plot1.R

---
+++ assigment 1/Plot1.R
@@ -0,0 +1,33 @@
+rm(list = ls())
+library(hms)
+library(dplyr)
+Sys.setlocale("LC_ALL", "English") # for English weekday
+Sys.setenv("LANGUAGE"="En")
+
+My_data<-read.table("./exdata_data_household_power_consumption/household_power_consumption.txt",
+                    sep=";",header=TRUE,
+                    stringsAsFactors = FALSE) #load dataset
+
+My_data$Date<-as.Date(My_data$Date, format='%d/%m/%Y')
+
+
+My_data<-filter(My_data, Date=="2007-02-01" | Date=="2007-02-02")
+My_data$Time<-as_hms(My_data$Time)
+My_data$Global_active_power<-as.numeric(My_data$Global_active_power)
+My_data$Global_reactive_power<-as.numeric(My_data$Global_reactive_power)
+My_data$Voltage<-as.numeric(My_data$Voltage)
+My_data$Global_intensity<-as.numeric(My_data$Global_intensity)
+My_data$Sub_metering_1<-as.numeric(My_data$Sub_metering_1)
+My_data$Sub_metering_2<-as.numeric(My_data$Sub_metering_2)
+
+My_data<-mutate((My_data), date_time=paste(Date, Time, sep=" "))
+
+My_data$date_time<-as.POSIXlt(My_data$date_time)
+
+
+png("plot1.png", width=480, height=480)
+hist(My_data$Global_active_power, xlab = "Global Active Power (kilowatts)", main="Global Active Power", col="red")
+dev.off()
+
+
+
